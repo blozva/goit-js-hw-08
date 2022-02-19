86296930def0e7df726bfed8ff851d48d29396a6
Ruslana Blozva <runa.blozva@gmail.com>
2022-02-19 21:11:26 +0200

    Галерея

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,22 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const gallery  = document.querySelector('div.gallery');
+const pictureElements = galleryItems.map(
+    img => `<a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}">
+    </a>`
+).join('');
+gallery.insertAdjacentHTML("beforeend", pictureElements);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
